f1545fc6689b4f132921fc9b58c0783aa6656159
ThiagoMdO <thiago_catureba@hotmail.com>
2024-07-08 18:17:25 -0300

    fix: added field relation between Customer with ListAddress, by OneToMany anotation

--- MS-Customer/src/main/java/com/MS_Customer/entities/Customer.java
+++ MS-Customer/src/main/java/com/MS_Customer/entities/Customer.java
@@ -1,10 +1,7 @@
 package com.MS_Customer.entities;
 
 import com.MS_Customer.enums.SexEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -15,6 +12,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
@@ -47,6 +45,9 @@ public class Customer {
     @Size(min = 6)
     @NotBlank
     private String password;
+
+    @OneToMany
+    private List<Address> addresses;
 
     private boolean active = true;
 }
