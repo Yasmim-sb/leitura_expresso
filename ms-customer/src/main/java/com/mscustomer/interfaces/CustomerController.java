ccb69131306eb9ff81943163bb5dd82e9b64de1f
Ryansousa10 <ryan.sousa4000@gmail.com>
2024-08-07 10:42:57 -0300

    fix: removing mscustomer duplicated

--- ms-customer/src/main/java/com/mscustomer/interfaces/CustomerController.java
+++
@@ -1,9 +0,0 @@
-package com.MS_Customer.interfaces;
-
-import com.MS_Customer.request.CustomerNewPasswordRequest;
-import org.springframework.http.ResponseEntity;
-
-public interface CustomerController {
-
-    //public ResponseEntity<Void> changePasswordCustomer(Long id, CustomerNewPasswordRequest newPasswordDTO);
-}
