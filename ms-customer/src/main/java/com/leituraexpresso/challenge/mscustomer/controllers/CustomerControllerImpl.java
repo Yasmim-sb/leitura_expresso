2e2cb5f30f994844c56af11b2db509816d52d983
ThiagoMdO <thiago_catureba@hotmail.com>
2024-08-19 19:45:12 -0300

    fix: tests authentication in AddressController layer

--- ms-customer/src/main/java/com/leituraexpresso/challenge/mscustomer/controllers/CustomerControllerImpl.java
+++ ms-customer/src/main/java/com/leituraexpresso/challenge/mscustomer/controllers/CustomerControllerImpl.java
@@ -40,7 +40,10 @@ public class CustomerControllerImpl implements CustomerController {
 
     @Override
     @PutMapping("/customers/{id}/password")
-    public ResponseEntity<Void> changePasswordCustomer(@PathVariable Long id, @AuthenticationPrincipal UserDetails customerAuthenticated, @RequestBody @Valid CustomerNewPasswordRequest newPasswordRequest) {
+    public ResponseEntity<Void> changePasswordCustomer(
+    @PathVariable Long id,
+    @AuthenticationPrincipal UserDetails customerAuthenticated,
+    @RequestBody @Valid CustomerNewPasswordRequest newPasswordRequest) {
         customerService.updatePassword(id, customerAuthenticated, newPasswordRequest);
         return ResponseEntity.noContent().build();
     }
--- ms-customer/src/test/java/com/leituraexpresso/challenge/mscustomer/web/AddressControllerImplTests.java
+++ ms-customer/src/test/java/com/leituraexpresso/challenge/mscustomer/web/AddressControllerImplTests.java
@@ -1,30 +1,45 @@
 package com.leituraexpresso.challenge.mscustomer.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.leituraexpresso.challenge.mscustomer.controllers.AddressControllerImpl;
 import com.leituraexpresso.challenge.mscustomer.exceptions.customExceptions.AddressNotFoundException;
 import com.leituraexpresso.challenge.mscustomer.services.AddressService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.context.WebApplicationContext;
 
 import static com.leituraexpresso.challenge.mscustomer.common.AddressConstants.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@WebMvcTest(AddressControllerImpl.class)
+//@WebMvcTest(AddressControllerImpl.class)
+@SpringBootTest
 class AddressControllerImplTests {
 
-    @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @BeforeEach
+    public void setUp(){
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+        .apply(springSecurity())
+        .build();
+    }
 
     @Autowired
     ObjectMapper objectMapper;
@@ -38,6 +53,7 @@ class AddressControllerImplTests {
         when(addressService.create(ADDRESS01_REQUEST_CORRECT_FIELDS)).thenReturn(ADDRESS01_DTO);
 
         mockMvc.perform(post("/v1/address")
+        .with(user("Pedro").authorities(new SimpleGrantedAuthority("USER")))
         .content(objectMapper.writeValueAsString(ADDRESS01_REQUEST_CORRECT_FIELDS))
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isCreated());
@@ -49,6 +65,7 @@ class AddressControllerImplTests {
     void create_withInValidFields_ThrowMethodArgumentNotValidException_Status400() throws Exception{
 
         mockMvc.perform(post("/v1/address")
+        .with(user("Pedro").authorities(new SimpleGrantedAuthority("USER")))
         .content(objectMapper.writeValueAsString(ADDRESS01_REQUEST_INVALID_FIELDS))
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
@@ -61,6 +78,7 @@ class AddressControllerImplTests {
         when(addressService.update(2L, ADDRESS01_REQUEST_CORRECT_FIELDS)).thenReturn(ADDRESS01_DTO);
 
         mockMvc.perform(put("/v1/address/{id}", 2L)
+        .with(user("Pedro").authorities(new SimpleGrantedAuthority("USER")))
         .content(objectMapper.writeValueAsString(ADDRESS01_REQUEST_CORRECT_FIELDS))
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk());
@@ -71,6 +89,7 @@ class AddressControllerImplTests {
     void update_withInvalidFields_ThrowMethodArgumentNotValidException_Status400() throws Exception {
 
         mockMvc.perform(put("/v1/address/{id}", 2L)
+        .with(user("Pedro").authorities(new SimpleGrantedAuthority("USER")))
         .content(objectMapper.writeValueAsString(ADDRESS01_REQUEST_INVALID_FIELDS))
         .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isBadRequest())
@@ -83,7 +102,9 @@ class AddressControllerImplTests {
     void delete_withAddressIdValid_ReturnsVoid_Status_204() throws Exception {
         doNothing().when(addressService).delete(ADDRESS01_DTO.getId());
 
-        mockMvc.perform(delete("/v1/address/{id}", ADDRESS01_DTO.getId()))
+        mockMvc.perform(delete("/v1/address/{id}", ADDRESS01_DTO.getId())
+        .with(user("Pedro").authorities(new SimpleGrantedAuthority("USER")))
+        )
         .andExpect(status().isNoContent());
     }
 
@@ -92,7 +113,9 @@ class AddressControllerImplTests {
     void delete_withAddressIdValidInvalid_ThrowAddressNotFoundException_Status_404() throws Exception {
         doThrow(AddressNotFoundException.class).when(addressService).delete(99L);
 
-        mockMvc.perform(delete("/v1/address/{id}", 99L))
-                .andExpect(status().isNotFound());
+        mockMvc.perform(delete("/v1/address/{id}", 99L)
+        .with(user("Pedro").authorities(new SimpleGrantedAuthority("USER")))
+        )
+        .andExpect(status().isNotFound());
     }
 }
